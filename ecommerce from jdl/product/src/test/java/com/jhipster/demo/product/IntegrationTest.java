eccebf60557332849e7152c49beb3004ef112a3b
parasuram <parasuram.tanguturu@siemen.com>
2021-11-01 08:27:32 +0530

    Initial version of product generated by generator-jhipster@7.3.1

---
+++ ecommerce from jdl/product/src/test/java/com/jhipster/demo/product/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.jhipster.demo.product;
+
+import com.jhipster.demo.product.ProductApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = ProductApp.class)
+public @interface IntegrationTest {
+}
